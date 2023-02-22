30066813690074d7addf6a8a9a6e4416be337960
Teoteo11 <depaym69@gmail.com>
2023-02-22 10:02:02 +0100

    feat(second.ts): add callback sum
    feat(second.ts): add minus callback
    
    parent - change 1
    
    parent - change 2
    
    parent - change 4
    
    child - change 1
    
    child - change 2

---
+++ second.ts
@@ -0,0 +1,11 @@
+// add
+const add = (a: number, b: number ) => a + b;
+
+// minus
+const minus = (a: number, b: number) => a - b;
+
+
+// parent comment
+// start to another branch
+
+// another comment
